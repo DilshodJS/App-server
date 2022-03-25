4f7c79aac86e14e1e075d9d921765496c702317a
DilshodJS <dilshoddev6969@gmail.com>
2022-03-25 15:55:59 +0500

    start param

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import express, { response } from "express"
+import e from "express";
+import express, { query, response } from "express"
 import { request } from "http";
 import path,{ dirname} from "path";
 
@@ -15,39 +16,61 @@ app.get('/',(request,response)=>{
 
 //post zapros 
 
-const Option={
-  root:path.join(__dirname,'../pages/')
-}
+// const Option={
+//   root:path.join(__dirname,'../pages/')
+// }
 
-app.get('/data',(request,response)=>{
-  response.sendFile("index.html",Option)
-})
+// app.get('/data',(request,response)=>{
+//   response.sendFile("index.html",Option)
+// })
 
-app.get('/nav2',(request,response)=>{
-  response.sendFile("Home2.html",Option)
-})
-
-
-
-app.get('/html',(request,response)=>{
-  response.send(`<h1>Heading tags in htmlllll</h1>`)
-})
+// app.get('/nav2',(request,response)=>{
+//   response.sendFile("Home2.html",Option)
+// })
 
 
-app.get('/json',(request,response)=>{
-  response.send({
-    username:'diwqaa',
-    age:20,
-    username2:'Abdullayev'
+
+// app.get('/html',(request,response)=>{
+//   response.send(`<h1>Heading tags in htmlllll</h1>`)
+// })
+
+
+// app.get('/json',(request,response)=>{
+//   response.send({
+//     username:'diwqaa',
+//     age:20,
+//     username2:'Abdullayev'
+//   })
+// })
+
+
+
+// app.get('/add',(req,res)=>{
+//   let a=+req.query.a!
+//   let b=+req.query.b!
+//    res.send("javab "+(a+b))
+// })
+
+
+app.get('/add',(req,res)=>{
+  let a=+req.query.a!
+  let b=+req.query.b!
+  let c=+req.query.c!
+   let sum=Math.max(a,b,c)
+   res.send("javab"+sum)
   })
-})
 
 
+  
+app.get('/welcome',(req,res)=>{
+  let name=req.query.name
+  let surname=req.query.surname
+
+   res.send(`Welcome  ${name} ${surname}`)
+ 
+  })
 
 
-
-
-
-app.listen(port,()=>{
+  app.listen(port,()=>{
   console.log("Server ishladi http://127.0.0.1"+ port);
 });
