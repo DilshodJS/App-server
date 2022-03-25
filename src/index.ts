57fb6de8cace46bd200b3425b6c18c5a50694027
DilshodJS <dilshoddev6969@gmail.com>
2022-03-25 15:17:59 +0500

    start path

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,53 @@
-import prompt_sync from "prompt-sync";
-const prompt = prompt_sync()
+import express, { response } from "express"
+import { request } from "http";
+import path,{ dirname} from "path";
 
-const value = prompt('Enter value:')
+const app=express();
 
-console.log("your value: " + value);
+const port=3000;  
+
+//get zapros 
+
+app.get('/',(request,response)=>{
+  response.send( "get zapros keldi.nima qilaman?")
+})
+
+
+//post zapros 
+
+const Option={
+  root:path.join(__dirname,'../pages/')
+}
+
+app.get('/data',(request,response)=>{
+  response.sendFile("index.html",Option)
+})
+
+app.get('/nav2',(request,response)=>{
+  response.sendFile("Home2.html",Option)
+})
+
+
+
+app.get('/html',(request,response)=>{
+  response.send(`<h1>Heading tags in htmlllll</h1>`)
+})
+
+
+app.get('/json',(request,response)=>{
+  response.send({
+    username:'diwqaa',
+    age:20,
+    username2:'Abdullayev'
+  })
+})
+
+
+
+
+
+
+
+app.listen(port,()=>{
+  console.log("Server ishladi http://127.0.0.1"+ port);
+});
